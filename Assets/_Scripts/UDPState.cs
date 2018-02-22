cc990dd577dff10c8d8713d3b706f0bc76081a1b
Nick Bucher <admin@nicholasbucher.com>
2018-02-22 09:38:02 -0600

    initial commit

---
+++ Assets/_Scripts/UDPState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+
+public class UdpState
+{
+    public UdpClient udpClient;
+    public IPEndPoint ipEndPoint;
+    public const int BufferSize = 1024;
+    public byte[] buffer = new byte[BufferSize];
+    public int counter = 0;
+}
