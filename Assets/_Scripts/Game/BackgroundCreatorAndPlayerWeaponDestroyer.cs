7c4e8f34cd6bd2c1145c565ac408e97f0b230772
Nick Bucher <admin@nicholasbucher.com>
2018-03-02 18:33:56 -0600

    updated ships lasers, started adding redflyingmonster, fixed grass generation

---
+++ Assets/_Scripts/Game/BackgroundCreatorAndPlayerWeaponDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundCreatorAndPlayerWeaponDestroyer : MonoBehaviour {
+    public GameObject grassRow;
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.tag == "BackgroundGrass")
+        {
+            var g = Instantiate(grassRow, transform.position, transform.rotation, transform) as GameObject;
+            g.transform.position = coll.transform.position + new Vector3(0,2,0);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "PlayerWeapon")
+            Destroy(coll.gameObject);
+    }
+
+}
