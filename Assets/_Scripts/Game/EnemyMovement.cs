d3cf665ca948b64f21dcd281580b74e3c0c16408
mlivingston <mdlivingston@crimson.ua.edu>
2018-03-02 18:47:14 -0600

    - Added scene - Imported enemy ship - Experimenting with enemy movement

---
+++ Assets/_Scripts/Game/EnemyMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+
+    public float speed = 5f;
+    public float mag;
+    // Update is called once per frame
+    void Update()
+    {
+        var p = transform.localPosition;
+        transform.Translate(new Vector3(Mathf.Sin(Time.time) * mag, speed * Time.deltaTime, 0));
+    }
+}
